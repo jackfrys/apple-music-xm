4c799cbad2663c883e910455920ae83a4d6dd80d
Jack Frysinger <frysinger.j@husky.neu.edu>
2017-09-21 16:57:23 -0400

    Initial commit

---
+++ apple-music-xm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  apple-music-xm
+//
+//  Created by Jack Frysinger on 9/21/17.
+//  Copyright © 2017 Jack Frysinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ apple-music-xmTests/apple_music_xmTests.swift
@@ -0,0 +1,36 @@
+//
+//  apple_music_xmTests.swift
+//  apple-music-xmTests
+//
+//  Created by Jack Frysinger on 9/21/17.
+//  Copyright © 2017 Jack Frysinger. All rights reserved.
+//
+
+import XCTest
+@testable import apple_music_xm
+
+class apple_music_xmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
